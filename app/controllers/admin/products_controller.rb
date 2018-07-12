951a4af0d5c11d1c479dfff1f1eb926b28462e6e
mspink <arpinkchen@gmail.com>
2018-07-12 21:31:56 +0800

    admin can upload mutiple photos for products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < Admin::BaseController
-   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
@@ -7,16 +7,23 @@ class Admin::ProductsController < Admin::BaseController
 
   def new
     @product = Product.new
+    @photo = @product.photos.build
 
+    #上傳圖
   end
 
   def show
-
+    @photos = @product.photos.all
   end
 
   def create
-    @product = Product.new(product_params) 
-    if @product.save     
+    @product = Product.new(product_params)
+    if @product.save  
+    if params[:photos] != nil
+      params[:photos]['avatar'].each do |a|
+        @photo = @product.photos.create(:avatar => a)                
+          end
+      end         
       flash[:notice]= "Product was successfully created"
       redirect_to admin_products_path
     else
@@ -26,6 +33,14 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def update
+    if params[:photos] != nil
+      @product.photos.destroy_all  #首先要删除旧的图片
+           
+      params[:photos]['avatar'].each do |a|
+        @photo = @product.photos.create!(:avatar => a)
+      end
+    end
+    
     if @product.update(product_params)
 
       flash[:notice]= "Product was successfully update"
@@ -50,6 +65,6 @@ class Admin::ProductsController < Admin::BaseController
     end
     
     def product_params
-      params.require(:product).permit(:name, :en_name, :description, :price, :flavor)
+      params.require(:product).permit(:name, :en_name, :description, :price, :flavor, :photo_id, photos_attributes: [:id, {avatar:[]}] )
     end
 end
