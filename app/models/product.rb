14ffbdbf05e6695142bc303d44ede928d8d65630
mspink <arpinkchen@gmail.com>
2018-07-12 21:31:02 +0800

    add product and photo relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates_presence_of :name
+  has_many :photos, dependent: :destroy
+
+  accepts_nested_attributes_for :photos, :allow_destroy => true
+  #之後要做nested form，先設定接受變更photo底下的attribute
 end
