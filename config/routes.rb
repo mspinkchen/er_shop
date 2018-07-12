2e321ea3c66d361e51b7163261cc4c21d4ef395e
mspink <arpinkchen@gmail.com>
2018-07-12 20:37:16 +0800

    add frontend and backend page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   root "home#index"
 
-    namespace :admin, path: "en1070416" do
+  resources :products, only: [:index, :show] 
+  resources :bulletins, only: [:index, :show]
+  resources :abouts,only: [:index]
+  resources :contacts 
+
+
+  namespace :admin, path: "en1070416" do
     resources :products
     root "products#index"
+    resources :bulletins
+    resources :categories
+    resources :contacts, only: [:index, :destroy]
   end
+
+
 
 end
