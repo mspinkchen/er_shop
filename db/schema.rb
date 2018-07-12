bd7c9a8d79db1f4c10d87987f20a36ca95a3aa03
mspink <arpinkchen@gmail.com>
2018-07-12 21:22:57 +0800

    add photo model and mount uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712124327) do
+ActiveRecord::Schema.define(version: 20180712132041) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "avatar"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
