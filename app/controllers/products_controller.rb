2bc3fd83cd816675e9259180fbc1b2eff1ef7cc4
mspink <arpinkchen@gmail.com>
2018-07-12 22:39:11 +0800

    add product index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:show]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @photos = @product.photos.all
+    @products = Product.where.not(params[:id])
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
