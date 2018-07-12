60fb365d368736dfe8d522f460bc308bc9d7d781
mspink <arpinkchen@gmail.com>
2018-07-12 20:48:48 +0800

    gem devise and simple_form and add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home#index"
 
   resources :products, only: [:index, :show] 
   resources :bulletins, only: [:index, :show]
   resources :abouts,only: [:index]
-  resources :contacts 
+  resources :contacts do
+    collection do
+      get :delivery 
+    end
+  end
+  get "/contacts/delivery" => "contacts#delivery"
+
 
 
   namespace :admin, path: "en1070416" do
